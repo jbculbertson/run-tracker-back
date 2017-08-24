0afaa076b2b889011463dcc0ae2deb689a6e792d
jbculbertson <jbculbertson@gmail.com>
2017-08-24 05:47:39 -0400

    Made first and last name required

--- app/models/user.js
+++ app/models/user.js
@@ -12,11 +12,11 @@ const userSchema = new mongoose.Schema({
   },
   firstName: {
     type: String,
-    required: false
+    required: true
   },
   lastName: {
     type: String,
-    required: false
+    required: true
   },
   token: {
     type: String,
